995a433b2f38211331a1f13ba0ae2b83a8068e1b
0kron <88357657+0kron@users.noreply.github.com>
2022-01-11 10:23:15 -0600

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,4 +6,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 #Launch the bar
 polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar secondary 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar secondary 2>&1 | tee -a /tmp/polybar1.log & disown #Comment this line if there is no second monitor
